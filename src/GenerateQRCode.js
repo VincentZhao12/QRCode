9c008bef809f486adccc93dade63cf5e2b1b45b1
VincentZhao12 <zhao.wentao.vincent@gmail.com>
2020-11-17 21:22:21 -0800

    organized folders

--- src/GenerateQRCode.js
+++ src/GenerateQRCode.js
@@ -3,5 +3,6 @@ var submit = document.getElementById("gen");
 var img = document.getElementById("qr-code");
 
 submit.onclick = function() {
-    img.src = "https://api.qrserver.com/v1/create-qr-code/?data=" + data.value + "&amp;size=100x100";
+    if(data.value.length > 0)
+        img.src = "https://api.qrserver.com/v1/create-qr-code/?data=" + data.value + "&amp;size=100x100";
 }
